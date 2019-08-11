11e09076919aefd5cf9ea57c885d1f30da7df1c2
glorious-genre-8033 <namontesino@gmail.com>
2019-08-11 00:59:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,9 +9,9 @@ def second_challenge
     :capulet => {
       patriarch: {},
       matriarch: {},
-      hero: {},
-      hero_friends: []
-      }}
-
+      heroine: {},
+      heroine_friends: []
+      }
+  }
   
 end
